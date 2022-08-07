7cc2174a6df62cef20df9b2e5a5dda8d19a21dc0
José Bernardino Jr <jbernardino.jr@gmail.com>
2022-08-07 03:08:19 -0300

    [asteroidRadarNasa] Adjusts from Code review

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -5,5 +5,5 @@ object Constants {
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
     const val DB_NAME = "asteroid_db"
-    const val API_KEY = "c6eEPhlArzpgVqIgCfazNX5gcWdCqIjPwDgx4hvf"
+    const val API_KEY = BuildConfig.NASA_API_KEY
 }
